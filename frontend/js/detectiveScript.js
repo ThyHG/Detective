9f611ec14798e54a40dab75dcd26bef2ac59852d
ThyHG <jonspruyt@gmail.com>
2016-04-10 14:33:05 +0200

    added support for creating cards out of json file

--- frontend/js/detectiveScript.js
+++ frontend/js/detectiveScript.js
@@ -7,14 +7,104 @@ var fake_questionnairre = {
 	'q5': 'What\'s the purpose of life?'
 }
 
-var init = function() {
+var fake_cards = [
+	{
+		'id': '1',
+		'answers': {
+			'a1': 'yes',
+			'a2': 'blue',
+			'a3': 'it\'s pretty',
+			'a4': 'of course',
+			'a5': 'I feel like it\'s more of a thing rather than a thing thing but more over less than equal'
+		}
+	},
+	{
+		'id': '2',
+		'answers': {
+			'a1': 'yes',
+			'a2': 'red',
+			'a3': 'it\'s pretty',
+			'a4': 'of course',
+			'a5': 'I feel like it\'s more of a thing rather than a thing thing but more over less than equal'
+		}
+	},
+	{
+		'id': '3',
+		'answers': {
+			'a1': 'yes',
+			'a2': 'yellow',
+			'a3': 'it\'s pretty',
+			'a4': 'of course',
+			'a5': 'I feel like it\'s more of a thing rather than a thing thing but more over less than equal'
+		}
+	}
+];
+
+$(document).ready(function() {
 	for (var key in fake_questionnairre) {
 	    if (Object.prototype.hasOwnProperty.call(fake_questionnairre, key)) {
 	        var val = fake_questionnairre[key];
 	        $('#start-questionnairre').append('<label>'+ val +'<input type="text" name="'+key+'"></label>')
 	    }
 	}
+	// TODO change this into code thinger.
+	$('#start-questionnairre').append('<button id="submit-questionnairre" type="button">Submit</button>');
+
+	// Actions
+	// On submission of questionnairre
+	// TODO check field validation (everything filled in)
+	$('#submit-questionnairre').on('click', function(event) {
+		event.preventDefault();
+		// TODO Do ajax request
+		// SEND ID of person + answers
+		$('#loading').show();
+		$('#start-questionnairre').hide();
+		window.setTimeout(showCards, 1000)
+	})
+});
+
+showCards = function() {
+	//Hide waiting screen
+	$('#loading').hide();
+
+	//New card instance (also used as 'subcards')
+	//Data is directly from the cards object from server
+	var Card = function(data) {
+    	this.data = data;
+	};
+
+	//Add the render function to the prototype of card instance
+	//Container: The parent element
+	//Headrender: Is the to be rendered element a card or an answer?
+	Card.prototype.render = function(container, headrender) {
+		if(headrender){
+			//If new card
+	    	var card = $('<div data-id=' + this.data.id + ' class="card"></div>');
+	    	card.appendTo(container);
+	    	//Recursive call, the card needs to be filled with answers
+	    	Card.renderCards(this.data.answers, card, false);
+		} else {
+			//If answers
+		    var p = $("<p></p>", {
+		    	text: this.data
+		    });
+	      	p.appendTo(container);
+		}
+	};
+	//renderCards - Render cards or answers on them for each element in the object passed
+	//cards: the javascript object that needs to be displayed
+	//container: the parent object
+	//headrender: Is it a card or answers to be passed along to render function.
+	Card.renderCards = function(cards, container, headrender) {
+	    $.each(cards, function(key, val) {
+	        var c = new Card(val);
+	        c.render(container, headrender);
+	    });
+	}
+	//Create the cards.
+	Card.renderCards(fake_cards, $("#cards"), true);
 }
+
 /* 
 // 1. Get questionnairre from server
 	// a. fake question set and put into form
