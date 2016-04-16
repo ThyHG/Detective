36a4f527ac45121c6ac36a8f7b7eae74555e5df7
Dennis Ku <dennis.ku88@gmail.com>
2016-04-16 19:17:05 +0200

    fixed graphical bug, delete bug, now updates playercount, changed to filename to j.on, basic tweaking and feedback

--- backend/admin.php
+++ backend/admin.php
@@ -1,66 +1,51 @@
 <?php
 
 //game status, true = running, false = offline
-$game_running = file_exists("on");
+$game_running = file_exists("j.on");
+
+$player_count = count(glob("data/*"));
+
+//for msgs
+$notice = "";
 
 if(isset($_GET["start"])){
 
 	if(!$game_running){
 
 		//create file to indicate game state
-		touch("on");
-		
+		touch("j.on");
+
+		$notice = "Started!";
+
 	} else{
 
-		echo "Game already running";
+		$notice = "Game already running!";
 
 	}
 	
 } elseif(isset($_GET["stop"]) && $game_running){
+
 	$game_running = false;
 
 	//delete "on" file
-	unlink("on");
+	unlink("j.on");
 
 	//delete game data i.e. client questions
-	array_map("unlink", glob("data/*.*"));
+	array_map("unlink", glob("data/*"));
 
-}
-/*
-if(isset($_GET["id"]) && $settings->status == "on"){
-	
-} else{
-		return "Access denied";
+	$notice = "Game stopped!";
 }
 
-if($settings->status == "off"){
-	return "Game isn't running";
-}
-*/
+//front end
+include("admin_template.php");
 
-?>
-<html>
-	<table>
-		<tr>
-			<td>Game status</td>
-			<td><?php echo $game_running ? "online" : "offline"; ?></td>
-		</tr>
-		<tr>
-			<td>Players</td>
-			<td><?php  ?></td>
-		</tr>
-		<tr>
-			<td><a href="admin.php?start">Start game</td>
-			<td><a href="admin.php?stop">Stop game</td>
-		</tr>
-	</table>
-</html>
+header("Refresh:7; url=admin.php");
 
-<?php
 /*
  * for debugging
  */
 function dump($array) {
 	echo "<pre>" . htmlentities(print_r($array, 1)) . "</pre>";
 }
+
 ?>
