45bbe20337a9e69a76ff8230692b652079891a33
Dennis Ku <dennis.ku88@gmail.com>
2016-04-18 12:55:13 +0200

    tweaking and added functionality (details below) - save which cards the client received. assumption: ppl always get unique people (i.e. not duplicate card-ppl) - todo: endless loop if not enough people in the people pool. (error handling?) - automatically recognizes if requestor is first-time or is asking for more

--- backend/getCards.php
+++ backend/getCards.php
@@ -5,22 +5,32 @@ header('Content-Type: application/json');
 
 //read pool of cards, save who has gotten what
 
+//TODO: mark cards as solved? (for reconnection cases)
+
 //check if ID is being sent
 if( isset($_GET["id"]) ){
 
 	$id = $_GET["id"];
 
+	//check if person requests more or is a first time requestor
+	//if file exists, person is requesting for more = true
+	$more = file_exists("data/save/{$id}");
+
 	//TODO: decide on quanitity / set settings in admin.php
 	$card_qty = 2;
 	$fact_qty = 2;
 
-	$ppl_pool = glob("data/*");
+	//all ppl fact data
+	$ppl_pool = glob("data/facts/*");
 
+	//to be send to client
 	$cards = [];
 
+	//tracking which IDs were used to make cards
+	$track_ids = [];
+
 	//choose x cards
-	//USE COUNTER INSTEAD OF FOR?
-	for($i = 0; sizeof($cards) < $card_qty; $i++){
+	while( sizeof($cards) < $card_qty ){
 
 		//random number between 0 and size of array
 		$rand_c = rand(0, (sizeof($ppl_pool)-1) );
@@ -32,7 +42,15 @@ if( isset($_GET["id"]) ){
 		
 		//if data-file choosen does not belong to the requestor
 		if($file_id != $id){
-	
+			
+			//if requested for more, check previous data first to avoid duplicates
+			if( $more && isDuplicate($file_id, $id) ){
+
+				//skip rest
+				continue;
+
+			}
+
 			//contains all facts of 1 person
 			$facts = [];
 
@@ -66,6 +84,9 @@ if( isset($_GET["id"]) ){
 			//add to existing cards
 			$cards[] = $container;
 
+			//tracking id
+			$track_ids[] = $file_id;
+
 			//remove from pool to prevent duplicate
 			unset($ppl_pool[$rand_c]);
 
@@ -74,7 +95,14 @@ if( isset($_GET["id"]) ){
 		}
 	}
 
-	//echo json_encode($cards, JSON_PRETTY_PRINT);
+	//save data => which cards (id) did the requestor get
+	//automatically appends if file exists, else creates the file
+	$handle = fopen("data/save/{$id}", "a");
+	fwrite($handle, implode(PHP_EOL, $track_ids) );
+	fwrite($handle, PHP_EOL );
+	fclose($handle);
+
+	//send the cards
 	echo json_encode($cards);
 
 
@@ -85,6 +113,21 @@ if( isset($_GET["id"]) ){
 }
 
 //point handling php: mark cards as done?
+/**
+ *	Checks if ID was already used for requestor ID
+ *
+ *	@param id		id to check if it has been used
+ *	@param r_id		id of requestor, used to read its save file
+ */
+function isDuplicate($id, $r_id){
+
+	//read file
+	$file = file_get_contents("data/save/{$r_id}");
+	$save_file = explode(PHP_EOL, $file);
+
+	//check if its inside
+	return in_array($id, $save_file);
+}
 
 function dump($array) {
 	echo "<pre>" . htmlentities(print_r($array, 1)) . "</pre>";
