cd73523dc43cccd6a0a370d8ce5c54cc1286f824
Dennis Ku <dennis.ku88@gmail.com>
2016-04-18 19:58:22 +0200

    keeps track of the scores

---
+++ backend/score.php
@@ -0,0 +1,47 @@
+<?php
+
+//check if ID is being sent
+if( isset($_GET["id"]) ){
+	
+	$id = "".$_GET["id"];
+
+	//file for locking
+	$file = "data/score.json";
+
+	//c mode = read, write without truncating file
+	$handle = fopen($file, "c+");
+
+	//lock the file to prevent simultanous access
+	if(flock($handle, LOCK_EX)){
+		
+		//read score file
+		$file = fread($handle, filesize($file));
+		rewind($handle);
+		$json = json_decode($file);
+		
+		//update score
+		$json->{$id} += 1;
+
+		//dump($json);
+
+		//clear file
+		ftruncate($handle, 0);
+
+		//write changes to file
+		fwrite($handle, json_encode($json, JSON_PRETTY_PRINT));
+		
+		//flush output (=writes changes now instead of buffering)
+		fflush($handle);
+
+		//unlock file
+		flock($handle, LOCK_UN);
+	} 
+
+	//close file handle
+	fclose($handle);
+
+} else{
+	echo "herp no ID given";
+}
+
+?>
