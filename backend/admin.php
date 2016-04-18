09513359b7d791b3dda36bcd82e6ebd9fbe10511
Dennis Ku <dennis.ku88@gmail.com>
2016-04-18 19:57:45 +0200

    now creates scorefile based on players who requested questions (possible to change based on answered)

--- backend/admin.php
+++ backend/admin.php
@@ -22,6 +22,9 @@ if(isset($_GET["start"])){
 		//create file to indicate game state
 		touch("j.on");
 
+		//create score file for future scoring
+		startScore();
+
 		$notice = "Started!";
 
 	} else{
@@ -38,8 +41,12 @@ if(isset($_GET["start"])){
 	unlink("j.on");
 
 	//delete game data i.e. client facts and associated cards
+	//facts: collected answers per ID - save: keeping track which IDs were used for xy ID already
 	array_map("unlink", glob("data/facts/*"));
 	array_map("unlink", glob("data/save/*"));
+
+	//delete score file
+	unlink("data/score.json");
 
 	$notice = "Game stopped!";
 }
@@ -48,6 +55,31 @@ if(isset($_GET["start"])){
 include("admin_template.php");
 
 header("Refresh:7; url=admin.php");
+
+
+/**
+ * reads current players and creates a score file
+ */
+function startScore(){
+
+	//read player ids (= participatns)
+	$name_ids = array_map("basename", glob("data/facts/*"));
+
+	//data to write
+	$data = [];
+
+	//prepare data, {id: score}
+	foreach($name_ids as $id){
+		$data[$id] = 0;
+	}
+
+	//write (intitial) score file with 0 scores each player
+	$file = "data/score.json";
+	$handle = fopen($file, "w");
+	fwrite($handle, json_encode($data, JSON_PRETTY_PRINT));
+	fclose($handle);
+
+}
 
 /*
  * for debugging
